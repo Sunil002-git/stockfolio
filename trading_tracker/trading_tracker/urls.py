4b0651eaec1542c85970eaf39e8f578a6ead31cd
Sunil002-git <sunilmarri886@gmail.com>
2026-05-05 17:45:15 +0530

    added runtime.txt

--- trading_tracker/trading_tracker/urls.py
+++ trading_tracker/trading_tracker/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Backend is running 🚀"})
+
 urlpatterns = [
+    path('', home),   # 👈 ADD THIS
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
